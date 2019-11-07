804a738fdc95ea8377018639d03b0741569db0d6
Brendan Roy <br3ndanr@gmail.com>
2019-11-07 16:16:39 +1100

    add helloworld

--- hellomod.go
+++ hellomod.go
@@ -2,8 +2,12 @@ package hellomod
 
 import "fmt"
 
-const version = "2.2.0"
+const version = "2.3.0"
 
 func Hello() string {
 	return fmt.Sprintf("Hello, modules, version: %s", version)
+}
+
+func HelloWorld() string {
+	return "Hello, world!"
 }
