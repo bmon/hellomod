d8e3fedb5d74ade1eb02b69a25ef0c539eaf87f3
Brendan Roy <br3ndanr@gmail.com>
2019-11-07 14:42:49 +1100

    ver 2.1.1

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/bmon/hellomod
+module github.com/bmon/hellomod/v2
 
 go 1.13
--- hellomod.go
+++ hellomod.go
@@ -2,7 +2,7 @@ package hellomod
 
 import "fmt"
 
-const version = "2.1.0"
+const version = "2.1.1"
 
 func Hello() string {
 	return fmt.Sprintf("Hello, modules, version: %s", version)
